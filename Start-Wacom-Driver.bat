c1703b2aaead047c818df7e7cade0256d847d7f8
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2023-07-24 00:25:20 +0300

    Update Start-Wacom-Driver.bat

--- Start-Wacom-Driver.bat
+++ Start-Wacom-Driver.bat
@@ -1,3 +1,4 @@
-# Must be ran as administrator!
+@echo off
+rem Must be ran as administrator!
 powershell -Command "& {Get-PnpDevice -FriendlyName "*Wacom*" | Enable-PnpDevice -Confirm:$false}"
 net start WTabletServicePro
