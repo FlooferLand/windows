076cc1fef336ef29c5224cde4aa32c4eed4441fb
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2023-08-20 19:44:35 +0300

    Oops

--- Stop-Wacom-Driver.bat
+++ Stop-Wacom-Driver.bat
@@ -2,5 +2,5 @@
 
 rem Must be ran as administrator!
 powershell -Command "& {Get-PnpDevice -FriendlyName "*Wacom*" | Disable-PnpDevice -Confirm:$false}"
-sc config WTabletServicePro start=auto
+sc config WTabletServicePro start=demand
 net stop WTabletServicePro
