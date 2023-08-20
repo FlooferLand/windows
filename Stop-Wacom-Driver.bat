fe6bb0019206ab3a3a4b37724f742c000e7f1f79
FlooferLand! <76737186+FlooferLand@users.noreply.github.com>
2023-08-20 19:44:01 +0300

    Fixed start flag

--- Stop-Wacom-Driver.bat
+++ Stop-Wacom-Driver.bat
@@ -2,4 +2,5 @@
 
 rem Must be ran as administrator!
 powershell -Command "& {Get-PnpDevice -FriendlyName "*Wacom*" | Disable-PnpDevice -Confirm:$false}"
+sc config WTabletServicePro start=auto
 net stop WTabletServicePro
